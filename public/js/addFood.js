c094ac3d7bbed440457d27f4f51df24d7e0ddefb
Owen Treanor <realawesomebeary@gmail.com>
2025-12-03 16:57:11 -0500

    modified food buttons

--- public/js/addFood.js
+++ public/js/addFood.js
@@ -5,6 +5,7 @@ const dietaryAnalysisInfo = document.getElementById("dietaryAnalysisInfo");
 const goalAnalysisInfo = document.getElementById("goalAnalysisInfo");
 
 const addButton = document.getElementById("addButton");
+const nutButton = document.getElementById("nutButton");
 
 addButton.addEventListener("click", async (e) => {
   e.preventDefault();
@@ -23,7 +24,12 @@ addButton.addEventListener("click", async (e) => {
     });
     if (!response.ok) {
       console.log("error");
+    }else{
+        addButton.style.visibility = "hidden";
+        nutButton.style.visibility = "visible"
     }
+
+
   } catch (e) {
     console.log(error);
   }
